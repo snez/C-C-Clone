0f70ede4231a11d3b6faaa956ece9cfba666c2f3
MRdNk <duncanwilkie@hotmail.com>
2012-04-09 12:41:01 +0100

    Added pathfinding.js and other changes

--- assets/javascript/pathfinding.js
+++ assets/javascript/pathfinding.js
@@ -12,14 +12,15 @@ pathfinding.orthogonalScore = 10;
 pathfinding.grid = [];
 pathfinding.path = null;
 
-pathfinding.node = function (x,y) {
+/* An individual node (movement 'block'). */
+pathfinding.node = function (x, y, traversable) {
 	this.x = x;
 	this.y = y;
 	this.parentNode = null;
-	this.traversable = true;
-	this.g = 0; //Generated - Move cost from start node to this node
-	this.h = 0; //Heuristic - Move cost this node to destination node
-	this.f = 0; //g + h
+	this.traversable = (traversable === undefined || traversable === true) ? true : false;
+	this.g = 0; /*Generated - Move cost from start node to this node*/
+	this.h = 0; /*Heuristic - Move cost this node to destination node*/
+	this.f = 0; /*g + h*/
 }
 pathfinding.addStartNode = function (x, y) {
 	this.startNode = new this.node (x, y);
@@ -27,19 +28,27 @@ pathfinding.addStartNode = function (x, y) {
 pathfinding.addEndNode = function (x, y) {
 	this.endNode = new this.node (x, y);
 }
+/* Sets movement costs:
+		If orthogonal is undefined/null then uses default value of 10.
+		If diagonal is undefined/null then uses orthogonal * 1.4 (sqr of 2).
+			-- rounded as easier / quicker for JavaScript to calculate and acurate enough - consider revisiting. */
+pathfinding.setMovementCost = function (orthogonal, diagonal) {
+	this.orthogonalScore = (orthogonal === undefined || orthogonal === null) ? this.orthogonalScore : orthogonal;
+	this.diagonalScore = (diagonal === undefined || diagonal === null) ? Math.round(this.orthogonalScore * 1.4) : diagonal;
+}
 pathfinding.createGrid = function (x, y) {
 	for (var iX = 0; iX<x; iX++) {
 		var ar = [];
 		for (var iY = 0; iY<y; iY++) {
 			ar.push (new this.node (iX, iY));
 		}
-		pathfinding.grid.push (ar);
+		this.grid.push (ar);
 	}
 }
 pathfinding.alterGrid = function (x, y, trav) {
 	this.grid[x][y].traversable = trav;
 }
-// Finds if node is already in array based on x,y values.
+/* Finds if node is already in array based on x,y values. */
 pathfinding.indexOf = function (node, array) {
 	//console.log (node);
 	for (var i=0; i<array.length; i++) {
@@ -55,67 +64,35 @@ pathfinding.addToOpenset = function (node, parent) {
 	if ((node.traversable === true && indexOpen === -1 && indexClosed === -1) || (node.x === this.startNode.x && node.y === this.startNode.y)) {
 		if (parent !== undefined) {
 			//console.log ('parent: ', parent, node, parent.x, parent.y, node.x, node.y, indexClosed);
-			node.parentNode = parent; //Object.create(parent);
+			node.parentNode = parent;
 			node.g = this.calculateG (node);
 			node.h = this.calculateH (node);
 			node.f = node.g + node.h;
 			//console.log (node.g, node.h, node.f);
 		}
-		else {
-			node.parentNode = parent;
-		}
 		this.openset.push (node);
 	}
 	else if (indexOpen > -1 && indexClosed !== -1) {
 		
-		//console.log('addToOpenset-indexOpen>-1: ',node, this.calculateG(node));
 		if (this.calculateG (node) <= this.openset[indexOpen].g) {
 			node.parentNode = parent;
-			//console.log ('indexOpen > -1: ', node);
 			this.openset[indexOpen].g = this.calculateG (node);
 			this.openset[indexOpen].parentNode = parent;
 			this.openset[indexOpen].f = this.openset[indexOpen].g + this.openset[indexOpen].h;
 		}
 	}
 }
-
-// Calculates the g (generated) score => Move cost from start node to this node
-pathfinding.calculateG = function (node) {
-	var parent = node.parentNode;
-	var orth = this.orthogonalScore;
-	var diag = this.diagonalScore;
-	var g;
-	//if node has the same x or y value then movement is orthogonal otherwise it's diagonal movement.
-	//orthogonal movement = 10; diagonal movement = 14;
-	g = (node.x === parent.x || node.y === parent.y) ? orth + parent.g : diag + parent.g; 
-	g = (node.x === parent.x && node.y === parent.y) ? parent.g : g;
-	return g;
+pathfinding.addToClosedset = function (node) {
+	this.closedset.push(node)
 }
-// Calculates the h (heuristic) score => Move cost from a node to end node orthogonally.
-pathfinding.calculateH = function (node) {
-	var x = (this.endNode.x > node.x) ? this.endNode.x - node.x : this.endNode.x - node.x;
-	var y = (this.endNode.y > node.y) ? this.endNode.y - node.y : this.endNode.y - node.y;
-	//console.log ('calculateH x: ', node.x, this.endNode.x, x, '| calculateH y: ', node.y, this.endNode.y, y);
-	
-	var x = Math.abs(x) * this.orthogonalScore;
-	var y = Math.abs(y) * this.orthogonalScore;
-	var z = x + y;
-	//z = (z < 0) ? Math.abs(z) : z;
-	//console.log ('h: ',z);
-	return z;
-}
-//pathfinding.removeFromOpenset = function (node) {
 pathfinding.removeFromArray = function (node, array) {
-	var index = this.indexOf (node, array); //this.openset.indexOf (node);
+	var index = this.indexOf (node, array);
 	var removedNode = array[index];
-	//console.log('removedNode: ',removedNode);
 	array.splice (index, 1);
-	//console.log('removedNode: ',removedNode);
 	if (removedNode === undefined) {
 		return -1;
 	}
 	return removedNode;
-	//return this.openset;
 }
 pathfinding.removeFromOpenset = function (node) {
 	return this.removeFromArray (node, this.openset);
@@ -123,56 +100,9 @@ pathfinding.removeFromOpenset = function (node) {
 pathfinding.removeFromClosedset = function (node) {
 	this.removeFromArray (node, this.closedset);
 }
-pathfinding.addToClosedset = function (node) {
-	this.closedset.push(node)
-}
-pathfinding.getGridNode = function (node,x,y) {
-	if(x < 0  || y < 0) {
-
-	}
-	else {
-		var new_node = this.grid[x][y];
-		this.addToOpenset(new_node, node);
-
-	}
-}
-pathfinding.getAllAdjacentNodes = function (node) {
-	//var currentarray = [];
-	//console.log (node);
-	this.getGridNode(node, node.x-1, node.y-1);
-	this.getGridNode(node, node.x, node.y-1);
-	this.getGridNode(node, node.x+1, node.y-1);
-
-	this.getGridNode(node, node.x-1, node.y);
-	this.getGridNode(node, node.x+1, node.y);
-
-	this.getGridNode(node, node.x-1, node.y+1);
-	this.getGridNode(node, node.x, node.y+1);
-	this.getGridNode(node, node.x+1, node.y+1);
-/*
-	this.addToOpenset (this.grid[node.x-1][node.y-1], node);
-	this.addToOpenset (this.grid[node.x][node.y-1], node);
-	this.addToOpenset (this.grid[node.x+1][node.y-1], node);
-	
-	this.addToOpenset (this.grid[node.x-1][node.y], node);
-	//this.addToOpenset (this.grid[node.x][node.y], node);
-	this.addToOpenset (this.grid[node.x+1][node.y], node);
-
-	this.addToOpenset (this.grid[node.x-1][node.y+1], node);
-	this.addToOpenset (this.grid[node.x][node.y+1], node);
-	this.addToOpenset (this.grid[node.x+1][node.y+1], node);
-*/
-}
 pathfinding.moveFromOpensetToClosedset = function (node) {
-	var removedNode = this.removeFromOpenset (node);
-	//console.log ('removedNode: ', removedNode);
-	//this.addToClosedset (removedNode);
-	console.log(this.addToClosedset (node));
-	if (node.x === this.endNode.x && node.y === this.endNode.y) {
-		console.log (node);	
-		console.log (this.indexOf(node, this.closedset));
-		console.log (this.indexOf(node, this.openset));
-	}
+	this.removeFromOpenset (node);
+	this.addToClosedset (node);
 	if (node.x === this.endNode.x && node.y === this.endNode.y) {
 		this.path = node;
 		return 1;
@@ -181,25 +111,73 @@ pathfinding.moveFromOpensetToClosedset = function (node) {
 		return 0;
 	}
 }
+pathfinding.getGridNode = function (node, x, y) {
+	if(x < 0  || y < 0) {}
+	else {
+		var new_node = this.grid[x][y];
+		this.addToOpenset (new_node, node);
+	}
+}
+pathfinding.getAllAdjacentNodes = function (node) {
+	this.getGridNode (node, node.x-1, node.y-1);
+	this.getGridNode (node, node.x, node.y-1);
+	this.getGridNode (node, node.x+1, node.y-1);
+
+	this.getGridNode (node, node.x-1, node.y);
+	this.getGridNode (node, node.x+1, node.y);
+
+	this.getGridNode (node, node.x-1, node.y+1);
+	this.getGridNode (node, node.x, node.y+1);
+	this.getGridNode (node, node.x+1, node.y+1);
+}
+/* Calculates the g (generated) score => Move cost from start node to this node */
+pathfinding.calculateG = function (node) {
+	var parent = node.parentNode;
+	var orth = this.orthogonalScore;
+	var diag = this.diagonalScore;
+	var g;
+	/*if node has the same x or y value then movement is orthogonal otherwise it's a diagonal movement.
+	orthogonal movement = 10; diagonal movement = 14; */
+	g = (node.x === parent.x || node.y === parent.y) ? orth + parent.g : diag + parent.g; 
+	g = (node.x === parent.x && node.y === parent.y) ? parent.g : g;
+	return g;
+}
+/* Calculates the h (heuristic) score => Move cost from a node to end node orthogonally. */
+pathfinding.calculateH = function (node) {
+	var x = (this.endNode.x > node.x) ? this.endNode.x - node.x : this.endNode.x - node.x;
+	var y = (this.endNode.y > node.y) ? this.endNode.y - node.y : this.endNode.y - node.y;
+	
+	var x = Math.abs (x) * this.orthogonalScore;
+	var y = Math.abs (y) * this.orthogonalScore;
+	var z = x + y;
+
+	return z;
+}
 pathfinding.getLowestFScore = function () {
 	var lowestF = -1;
 	for (var i=0;i<this.openset.length;i++) {
 		lowestF = (lowestF === -1) ? this.openset[i] : lowestF;
-		lowestF = (this.openset[i].f <= lowestF.f) ? this.openset[i] : lowestF;
+		lowestF = (this.openset[i].f < lowestF.f) ? this.openset[i] : lowestF;
 	}
 	return lowestF;
+}
+pathfinding.getPathToArray = function () {
+	var arPath = [];
+	var node = this.path;
+
+	while (node.parentNode !== null) {
+		arPath.push (node);
+		node = node.parentNode;
+	}
+	return arPath.reverse();
 }
 pathfinding.start = function () {
 	this.addToOpenset (this.startNode);  				//1
 	this.getAllAdjacentNodes (this.startNode)  			//2
-	console.log (this.openset);
 	this.moveFromOpensetToClosedset (this.startNode);  	//3
-	console.log ('this.closedset: ',this.closedset, this.startNode);
 	
-
 	var done = 0;
 	while (done === 0) {
-		//console.log('while loop');
 		var lowestNode = this.getLowestFScore ();			//4
 		this.getAllAdjacentNodes (lowestNode);
 		done = this.moveFromOpensetToClosedset (lowestNode);
@@ -221,8 +199,9 @@ pathfinding.start = function () {
 
 //Instance
 var pf = Object.create (pathfinding);
-pf.addStartNode (1,1);
-pf.addEndNode (3,3);
+pf.setMovementCost(9);
+pf.addStartNode (3,3);
+pf.addEndNode (1,1);
 pf.createGrid (10,10);
 pf.alterGrid (2,0, false);
 pf.alterGrid (2,1,false);
@@ -239,5 +218,7 @@ pf.alterGrid (4,3,false);*/
 //pf.alterGrid (3,4,false);
 pf.start ();
 
-console.log (pf.startNode, pf.endNode);
-console.log ('grid', pf.grid);
+console.log ('startNode, endNode: ',pf.startNode, pf.endNode);
+console.log ('grid: ', pf.grid);
+console.log ('path: ', pf.path);
+console.log ('path array: ', pf.getPathToArray());
